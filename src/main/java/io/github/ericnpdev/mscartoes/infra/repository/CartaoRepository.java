8bdaa538d3801088479de88a9f643dbeb861801e
Eric Nunes de Paula <ericnpdev@gmail.com>
2023-05-22 23:25:52 -0300

    metodo findByRendaLessThanEqual

--- src/main/java/io/github/ericnpdev/mscartoes/infra/repository/CartaoRepository.java
+++ src/main/java/io/github/ericnpdev/mscartoes/infra/repository/CartaoRepository.java
@@ -4,5 +4,9 @@ package io.github.ericnpdev.mscartoes.infra.repository;
 import io.github.ericnpdev.mscartoes.domain.Cartao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface CartaoRepository extends JpaRepository<Cartao, Long> {
+    List<Cartao> findByRendaLessThanEqual(BigDecimal rendaBigdecimal);
 }
