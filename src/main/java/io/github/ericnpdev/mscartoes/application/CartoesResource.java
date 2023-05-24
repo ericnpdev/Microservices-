1f386919d9c5f5868bafdc4631a960805a38f3f1
Eric Nunes de Paula <ericnpdev@gmail.com>
2023-05-24 10:58:55 -0300

    add service, http status

--- src/main/java/io/github/ericnpdev/mscartoes/application/CartoesResource.java
+++ src/main/java/io/github/ericnpdev/mscartoes/application/CartoesResource.java
@@ -1,15 +1,31 @@
 package io.github.ericnpdev.mscartoes.application;
 
+import io.github.ericnpdev.mscartoes.application.representation.CartaoSaveRequest;
+import io.github.ericnpdev.mscartoes.domain.Cartao;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("cartoes")
+@RequiredArgsConstructor
 public class CartoesResource {
+
+    private final CartaoService service;
 
     @GetMapping
     public String status(){
         return "ok";
+    }
+
+    public ResponseEntity cadastra(@RequestBody CartaoSaveRequest request){
+        Cartao cartao = request.toModel();
+        service.save(cartao);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+
     }
 }
